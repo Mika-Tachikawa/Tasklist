f5b165749eb25a4468630777d69b8fc03d7f8634
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-07 06:15:58 +0000

    SpringBootの設定

--- Tasklist.java
+++ Tasklist.java
@@ -1,5 +1,9 @@
 package Java;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Tasklist {
   public static void main(String[] args) {
     for (int i=3; i>0; i--){
